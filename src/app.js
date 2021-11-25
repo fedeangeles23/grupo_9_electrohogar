08653a0ecce9e2b7d7da3c33755d8e5f3be393ed
JoaquinLGerez <joaquinlgerez@gmail.com>
2021-11-25 20:11:32 -0300

    trabajando

--- src/app.js
+++ src/app.js
@@ -17,36 +17,44 @@ http://localhost:${PORT}
 
 
 
+
+//View engine setup---
+
+app.set('views', path.join(__dirname, 'views'));
+
+app.set('view engine','ejs');
+
+
 //middlewares
 
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, '../public')));
 
 //------------------Rutas---------------------// 
  /* el metodo dentro de sendfile Join() busca la ruta en donde estamos, primer parametro __dirname hace referencia al directorio en donde estamos,
  el segundo parametro es el path relativo que queremos enviar, en este caso views/index.html */
 
 app.get('/', function (req, res){
-    res.render(path.join(__dirname, './src/views/products/home.ejs'))
+    res.render(path.join(__dirname, './views/products/home.ejs'))
 });
 
 app.get('/carrito', function (req, res){
-    res.sendFile(path.join(__dirname, './views/carrito.html'))
+    res.render(path.join(__dirname, './views/products/carrito.ejs'))
 });
 
 app.get('/login', function (req, res){
-    res.sendFile(path.join(__dirname, './views/login.html'))
+    res.render(path.join(__dirname, './views/login.ejs'))
 });
  
 app.get('/perfil', function (req, res){
-    res.sendFile(path.join(__dirname, './views/perfil.html'))
+    res.render(path.join(__dirname, './views/perfil.ejs'))
 });
 
 app.get('/productDetail', function (req, res){
-    res.sendFile(path.join(__dirname, './views/productDetail.html'))
+    res.sendFile(path.join(__dirname, './views/productDetail.ejs'))
 });
 
 app.get('/registro', function (req, res){
-    res.sendFile(path.join(__dirname, './views/registro.html'))
+    res.sendFile(path.join(__dirname, './views/registro.ejs'))
 });
 
 
