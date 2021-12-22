781e92b96d3cab68845ea19b3a75fc782684c073
JoaquinLGerez <joaquinlgerez@gmail.com>
2021-12-22 15:41:13 -0300

    m

--- src/app.js
+++ src/app.js
@@ -1,15 +1,9 @@
-// APP.JS es nuestro entry point.
-// npm init para iniciar, 
-//npm install express para que se cree la carpeta node_modules
-// codear los require express y path y asignar puerto 
-
-// express se instala en cada proyecto por separado 
-
 const express = require('express');
 const app = express();
 const PORT = 5000;
 const path = require('path')
 const method0verride =  require('method-override'); // Pasar poder usar los métodos PUT y DELETE
+const session = require('express-session')
 
 
 app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}
@@ -17,23 +11,31 @@ http://localhost:${PORT}
 `));
 
 
-//View engine setup---
+//---------View engine setup------------------
 
-app.set('views', path.join(__dirname, 'views')); //Ubicacion de la carpeta views
+app.set('views', path.join(__dirname, 'views')); // 
 
 app.set('view engine','ejs'); // setea que el view engine sera EJS 
 
 
-//middlewares
+//----------------Middlewares---------------
 
 app.use(express.static(path.join(__dirname, '../public')));
 //Sirve para solicitudes en donde se envian datos POST y PUT
 app.use(express.urlencoded({ extended: false }));
 //para trabajar con forms
 app.use(express.json());
-app.use(method0verride('__method'))
+app.use(method0verride('_method'));
+app.use(session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {secure: true}
+}))
 
-/* Enrutadores */
+
+
+/*-------------------Enrutadores--------------- */
 
 let login = require('./routes/loginRouter')
 let registro = require('./routes/registroRouter') 
@@ -46,7 +48,7 @@ let admin = require('./routes/adminRouter')
 
 let carrito = require ('./routes/cartRouter')
 
-/* ----------------------------------------- */
+/* ----------------Routes------------------------- */
 app.use('/', home);
 
 app.use('/carrito', carrito)
