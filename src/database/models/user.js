0d9e545a1940476462895d4bebe95fbc66ec3944
karenGonza <karengonza2001@gmail.com>
2022-02-02 00:08:08 -0300

    user.js en moduls

---
+++ src/database/models/user.js
@@ -0,0 +1,45 @@
+module.exports = (sequelize, dataTypes) => {
+    const alias = "user";
+    const cols = {
+      id: {
+          type: dataTypes.INTEGER(11).UNSIGNED,
+          primaryKey: true,
+          autoIncrement: true,
+          allowNull:false,
+      },
+          name: {
+              type: dataTypes.VARCHAR(80),
+              allowNull:false,
+          },
+          last_name: {
+            type: dataTypes.VARCHAR(80),
+            allowNull:false,
+          },
+          email: {
+            type: dataTypes.VARCHAR(70),
+            allowNull:false,
+            unique: true
+          },
+          pass: {
+            type: dataTypes.VARCHAR(60),
+            allowNull:false,
+          },
+          phone: {
+            type: dataTypes.VARCHAR(45).NULL
+          },
+          rol: {
+            
+          },
+          avatar: {
+            type: dataTypes.VARCHAR(100).NULL
+          }
+    }
+
+    const config = {
+        tableName: 'users'
+    }
+
+    const User = sequelize.define(alias, cols, config)
+
+    return User
+}
