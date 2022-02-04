b12bd17c3c8a16227326d7178d831062a9ba44a8
JoaquinLGerez <joaquinlgerez@gmail.com>
2022-02-03 22:59:56 -0300

    Models agregados

--- src/database/models/User.js
+++ src/database/models/User.js
@@ -1,86 +1,58 @@
-//Aca se define la tabla USERS donde se definen los modelos con sus tablas
-
 module.exports = (sequelize, dataTypes) => {
-    // El nombre con el cual llamaremos al modelo 'alias'
     const alias = "User";
-
-    //aca van las columnas de la tabla
     const cols = {
         id: {
-            //Va a aceptar 11 hasta caracteres unsigned
             type: dataTypes.INTEGER(11).UNSIGNED,
             primaryKey: true,
             autoIncrement: true,
             allowNull: false,
         },
-
         name: {
             type: dataTypes.STRING(45),
             allowNull: false,
         },
-
         last_name: {
             type: dataTypes.STRING(45),
             allowNull: false,
-        },
-
+        }, 
         email: {
             type: dataTypes.STRING(60),
             allowNull: false,
-            unique: true,
-        },
-
+            unique: true
+        }, 
         pass: {
             type: dataTypes.STRING(70),
             allowNull: false,
-        },
-
+        },  
         phone: {
-            type: dataTypes.STRING(70),
+            type: dataTypes.STRING(30),
         },
-
         rol: {
-
             type: dataTypes.INTEGER(2).UNSIGNED,
-            allowNull: false,
+            allowNull: false
         },
-
         avatar: {
-            type: dataTypes.STRING(100)
+            type: dataTypes.STRING(100), 
         }
-
 
     }
 
-
-    // Conectamos la tabla 
     const config = {
         tableName: 'users',
-
     }
 
     const User = sequelize.define(alias, cols, config)
 
-
-    //---------------- Relaciones----------------+
-
-
     User.associate = (models) => {
-        User.hasMany(models.Address, { //User tiene muchas direcciones
-            as: 'addresses', // Del usuario trae sus direcciones
-            foreignKey: 'userId', //La pk de la tabla a conectar
+        User.hasMany(models.Address, {
+            as: 'addresses',
+            foreignKey: 'userId',
         })
         User.hasOne(models.Order, {
             as: 'order',
             foreignKey: 'userId'
         })
-
     }
 
     return User
-
-
-}
-// ---------------------
-
-
+} 
