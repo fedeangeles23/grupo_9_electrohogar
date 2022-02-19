29d7d318a9771f772e88efe6e7ba8f4d1ee22397
karenGonza <karengonza2001@gmail.com>
2022-02-19 15:07:28 -0300

    modo nocturno en el home

---
+++ public/js/modoOscu.js
@@ -0,0 +1,54 @@
+window.onload = function(){
+//header  #140B5C  azul = #2d00f7 violeta = #a100f2
+//fondo del header
+let headerContainer = document.querySelector('.headercontainer');
+headerContainer.style.backgroundColor = '#0b090a';
+
+//lupa del header
+
+let buscar = document.querySelector('.fa-search');
+buscar.style.color = 'blue';
+/*
+let i = document.querySelector('type input');
+i.style.border = 'black';
+
+let placeholder = document.querySelector('placeholder input');
+placeholder.style.color = 'black'; */
+
+// nav 
+//fondo del nav
+let nav = document.querySelector('.nav');
+nav.style.backgroundColor = '#0b090a';
+
+//letras del nav
+let nav_list = document.querySelector('.nav_list');
+nav_list.style.color = '#a100f2';
+/*
+//lista del nav 
+let nav__listitemdrop = document.querySelector('.nav__listitemdrop');
+nav__listitemdrop.style.backgroundColor = '#0b090a';
+
+let a_drop = document.querySelectorAll('.a_drop');
+a_drop.style.color = 'blue';
+*/
+
+//home
+//Debajo del carrousel 
+//funciona 
+/*
+let lineabajocarousel = document.querySelector('.lineabajocarousel');
+lineabajocarousel.style.backgroundColor = '#0b090a' 
+*/
+//envios a todo el pais
+let col1linea = document.querySelector('.col1linea span');
+col1linea.style.color = '#a100f2';
+
+//paga con credito y debito
+let col2linea = document.querySelector('.col2linea span');
+col2linea.style.color = '#a100f2';
+
+//cosulta tus dudas al...
+let col3linea = document.querySelector('.col3linea span');
+col3linea.style.color = '#a100f2';
+} 
+
