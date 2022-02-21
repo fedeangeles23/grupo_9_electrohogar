09f381d363b30440edf6760eb3c508165f361493
karenGonza <karengonza2001@gmail.com>
2022-02-21 17:47:00 -0300

    mas modos oscuros

--- public/js/modoOscu.js
+++ public/js/modoOscu.js
@@ -1,103 +1,33 @@
 window.onload = function(){
-//header  #140B5C  azul = #2d00f7 violeta = #a100f2  negro = #0b090a
-//fondo del header
-let headerContainer = document.querySelector('.headercontainer');
-headerContainer.style.backgroundColor = '#0b090a';
-
-//lupa del header
-
-let buscar = document.querySelector('.fa-search');
-buscar.style.color = 'blue';
-/*
-let i = document.querySelector('type input');
-i.style.border = 'black';
-
-let placeholder = document.querySelector('placeholder input');
-placeholder.style.color = 'black'; */
-
-// nav 
-//fondo del nav
-let nav = document.querySelector('.nav');
-nav.style.backgroundColor = '#0b090a';
-
-//letras del nav
-let nav_list = document.querySelector('.nav_list');
-nav_list.style.color = '#a100f2';
-/*
-//lista del nav 
-let nav__listitemdrop = document.querySelector('.nav__listitemdrop');
-nav__listitemdrop.style.backgroundColor = '#0b090a';
-
-let a_drop = document.querySelectorAll('.a_drop');
-a_drop.style.color = 'blue';
-*/
-
-//home
-//Debajo del carrousel 
-//funciona 
-
-let lineabajocarousel = document.querySelector('.lineabajocarousel');
-lineabajocarousel.style.backgroundColor = '#0b090a' 
-
-//envios a todo el pais
-let col1linea = document.querySelector('.col1linea span');
-col1linea.style.color = '#a100f2';
-
-//paga con credito y debito
-let col2linea = document.querySelector('.col2linea span');
-col2linea.style.color = '#a100f2';
-
-//cosulta tus dudas al...
-let col3linea = document.querySelector('.col3linea span');
-col3linea.style.color = '#a100f2';
-
-//OFERTAS INPERDIBLES EN
-let H3 = document.querySelector('H3');
-H3.style.color = '#a100f2';
-
-
-
-let swiperSlide = document.querySelector('.swiper-slide');
-swiperSlide.style.backgroundColor = '#0b090a';
-
-let sectionSwiper = document.querySelector('.sectionSwiper');
-sectionSwiper.style.backgroundColor = '#0b090a';
-
-let containeHomerMarcas = document.querySelector('.containeHomerMarcas ');
-containeHomerMarcas.style.backgroundColor = '#0b090a';
-
-let SectionTittle = document.querySelector('.SectionTittle');
-SectionTittle.style.backgroundColor = '#0b090a';
-
-let swiper = document.querySelector('.imgcategorias');
-swiper.style.backgroundColor = '#0b090a';
-
-
- let separadorh3 = document.querySelector('.separadorh3 H3');
- separadorh3.style.color = '#a100f2';
-}
 
 //login
-{
 
+//funciona 
+/*
+ let containerregistro = document.querySelector('.containerregistro');
+ containerregistro.style.backgroundColor = '#0b090a';
+*/
 let ingreso = document.querySelector('.ingreso');
 ingreso.style.color = '#a100f2';
 
+let linklogin = document.querySelector('.link-login');
+linklogin.style.color = '#a100f2';
 
 
 }
 
-//registro
+//registro no funciona 
 {
     let logregistermain = document.querySelector('.logregistermain');
     logregistermain.style.backgroundColor = '#0b090a';
 
-    let ingreso = document.querySelector('.ingreso');
-     ingreso.style.color = '#a100f2';
-
     
-     let inputStyle = document.querySelector('.input-style placeholder');
-     inputStyle.style.backgroundColor = '#0b090a';
+
+     
+     let a_inicio = document.querySelector('.a_inicio');
+     a_inicio.style.color = '#a100f2';
+    
+    
  }
 
 
