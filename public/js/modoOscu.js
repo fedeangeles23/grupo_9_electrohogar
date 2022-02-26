a5bd94bd14c1ad9750645a894c52e6d67b663e68
karenGonza <karengonza2001@gmail.com>
2022-02-26 13:57:13 -0300

    retro 7

--- public/js/modoOscu.js
+++ public/js/modoOscu.js
@@ -1,23 +1,134 @@
 window.onload = function(){
 
+    //negro #0b090a  violeta #a100f2 
+    //fondo del header
+    let headerContainer = document.querySelector('.headercontainer');
+    headerContainer.style.backgroundColor = '#0b090a';
+    //fin del fondo del header 
+
+    let buscador = document.querySelector('.buscar input');
+    buscador.style.backgroundColor = '#0b090a';
+    
+    //lupa del header
+    let buscar = document.querySelector('.fa-search');
+    buscar.style.color = '#0b090a';
+    //fin de la lupa del header 
+    
+ /*
+    let i = document.querySelector('type input');
+    i.style.border = 'black';
+    
+    let placeholder = document.querySelector('placeholder input');
+    placeholder.style.color = 'black';  
+    */
+    
+    //fin de todo el header
+
+
+
+
+
+
+    // nav 
+  //fondo del nav
+  let nav = document.querySelector('.nav');
+  nav.style.backgroundColor = '#0b090a';
+  
+  //letras del nav
+  let nav_list = document.querySelector('.nav_list');
+  nav_list.style.color = '#a100f2';
+  
+  //lista del nav 
+  let nav__listitemdrop = document.querySelector('.nav__listitemdrop');
+  nav__listitemdrop.style.backgroundColor = '#0b090a';
+  
+ 
+
+
+
+
+
+
+
+
+
+    //home
+    //Debajo del carrousel 
+
+    let lineabajocarousel = document.querySelector('.lineabajocarousel');
+    lineabajocarousel.style.backgroundColor = '#0b090a'; 
+    
+
+    //envios a todo el pais
+    let col1linea = document.querySelector('.col1linea span');
+    col1linea.style.color = '#a100f2';
+    
+    //paga con credito y debito
+    let col2linea = document.querySelector('.col2linea span');
+    col2linea.style.color = '#a100f2';
+    
+    //cosulta tus dudas al...
+    let col3linea = document.querySelector('.col3linea span');
+    col3linea.style.color = '#a100f2'; 
+    
+    //OFERTAS INPERDIBLES EN
+    let H3 = document.querySelector('H3');
+    H3.style.color = '#a100f2'; 
+    
+    
+    
+    let swiperSlide = document.querySelector('.swiper-slide');
+    swiperSlide.style.backgroundColor = '#0b090a';
+    
+    let containeHomerMarcas = document.querySelector('.containeHomerMarcas ');
+    containeHomerMarcas.style.backgroundColor = '#0b090a';
+    
+    let SectionTittle = document.querySelector('.SectionTittle');
+    SectionTittle.style.backgroundColor = '#0b090a';
+    
+    let swiper = document.querySelector('.imgcategorias');
+    swiper.style.backgroundColor = '#0b090a';
+    
+     let separadorh3 = document.querySelector('H3');
+     separadorh3.style.color = '#a100f2';
+
+     let sectionSwiper = document.querySelector('.sectionSwiper');
+     sectionSwiper.style.backgroundColor = '#0b090a'; 
+
+
+     /* let SectionTittle = document.querySelector('.SectionTittle h3');
+      SectionTittle.style.color = '#a100f2';  */
+
+
+
+      
+     
+
+
+
+
+
+ 
+
 //login
 
 //funciona 
 /*
- let containerregistro = document.querySelector('.containerregistro');
- containerregistro.style.backgroundColor = '#0b090a';
-*/
-let ingreso = document.querySelector('.ingreso');
-ingreso.style.color = '#a100f2';
+let contenedorRegistro = document.querySelector('.containerregistro');
+ contenedorRegistro.style.backgroundColor = '#0b090a'; 
+
+ let ingreso = document.querySelector('.ingreso');
+ingreso.style.color = '#a100f2'; 
+
 
 let linklogin = document.querySelector('.link-login');
-linklogin.style.color = '#a100f2';
+linklogin.style.color = '#a100f2'; 
 
 
-}
 
+/*
 //registro no funciona 
-{
+
     let logregistermain = document.querySelector('.logregistermain');
     logregistermain.style.backgroundColor = '#0b090a';
 
@@ -27,7 +138,25 @@ linklogin.style.color = '#a100f2';
      let a_inicio = document.querySelector('.a_inicio');
      a_inicio.style.color = '#a100f2';
     
-    
+    */
+
+
+
+/*
+     //sobre nosotros
+     let sobreN = document.querySelector('.textSN');
+     sobreN.style.backgroundColor = '#0b090a'; */
+/*
+     let tituloSN = document.querySelector('.tituloSN');
+     tituloSN.style.color = 'azul'; 
+
+     let textoSN = document.querySelector('.textoSN');
+     textoSN.style.color = "azul"; */
+
+
+     //terminos y condiciones 
+     /* let texto = document.querySelector('.texto);
+     texto.style.color = 'azul'; */ 
  }
 
 
---
+++ public/js/validacionLogin.js
@@ -0,0 +1,60 @@
+function qs(element) {
+    return document.querySelector(element)
+}
+
+window.addEventListener('load', function () {
+    $email = qs('#email'),
+    $emailErrors = qs('#emailErrors'),
+    $pass = qs('#pass'),
+    $passErrors = qs('#passErrors'),
+    regExEmail = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i,
+    regExPass = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,12}$/;
+
+  let validationsErrors = false; 
+
+  $pass.addEventListener('blur', function(e){
+      let value = e.target.value
+    switch (true) {
+        case !value.trim():
+            $passErrors.innerHTML = 'La contraseña es obligatorio'
+            $pass.classList.add('is-invalid')
+            validationsErrors = true
+            break;
+        case !regExPass.test(value):
+            $passErrors.innerHTML = 'La contraseña debe tener: entre 6 o 12 caracteres, al menos una mayúscula, una minúscula y un número';
+            $pass.classList.add('is-invalid')
+            validationsErrors = true
+            break;    
+        default:
+            $pass.classList.remove("is-invalid");
+            $pass.classList.add('is-valid')
+            $passErrors.innerHTML = ""
+            validationsErrors = false
+            break;
+    }
+  })
+        /* $email.addEventListener('blur', function(e) { 
+            console.log(e) 
+            switch (true) {
+                case !$email.value.trim():
+                    $emailErrors.innerHTML = 'El email es obligatorio';
+                    $email.classList.add('is-invalid')
+                    validationsErrors = true
+                    break;
+            case ! regExEmail.test($email.value):
+            $emailErrors.innerHTML = 'debe tener un @';
+            $email.classList.add('is-invalid')
+            validationsErrors = true
+            break;
+                default:
+                    $email.classList.remove('is-invalid');
+                    $email.classList.add('is-valid')
+                    $emailErrors.innerHTML = ""
+                    validationsErrors = false
+                    break;
+            }
+        })
+    
+    }) */
+})
+ 
