f3f00ce95b64c83d4d7ddcb96d7273fb00b7b757
karenGonza <karengonza2001@gmail.com>
2022-03-07 15:10:44 -0300

    menu hamburguesa y vista de carrito

---
+++ public/js/subscripcion.js
@@ -0,0 +1,12 @@
+
+
+/* https://formsubmit.co/karengonza2001@gmail.com" method="post" */
+
+/* let sub = document.querySelector('#bbbSub');
+let btnSub = document.querySelector('#butSubcri');
+
+btnSub.addEventListener('click', function(e) {
+   /* sub.value  
+    console.log(`https://formsubmit.co/${sub.value}`)
+   /* https://formsubmit.co/karengonza2001@gmail.com" 
+}) */
